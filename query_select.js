071df01c1c36d5e12192554e80c983a238a985a2
Derkino <entreprise.julien.roy@gmail.com>
2023-10-30 00:28:35 +0100

    [Update] Preparation

--- query_select.js
+++ query_select.js
@@ -1,7 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/rank/top/:count', (req, res) => {
+router.get('/rank/top/level/:count', (req, res) => {
+    const count = req.params.count;
+    if (!/^\d+$/.test(count)) {
+        return res.status(400).json({ error: 'Invalid count parameter. Must be a number.' });
+    }
+    res.json({ message: `Selecting Top : ${count}` });
+});
+
+router.get('/rank/top/gold/:count', (req, res) => {
+    const count = req.params.count;
+    if (!/^\d+$/.test(count)) {
+        return res.status(400).json({ error: 'Invalid count parameter. Must be a number.' });
+    }
+    res.json({ message: `Selecting Top : ${count}` });
+});
+
+router.get('/rank/top/pkc/:count', (req, res) => {
+    const count = req.params.count;
+    if (!/^\d+$/.test(count)) {
+        return res.status(400).json({ error: 'Invalid count parameter. Must be a number.' });
+    }
+    res.json({ message: `Selecting Top : ${count}` });
+});
+
+router.get('/rank/top/arena/:count', (req, res) => {
     const count = req.params.count;
     if (!/^\d+$/.test(count)) {
         return res.status(400).json({ error: 'Invalid count parameter. Must be a number.' });
